5bfd15d4e324bb8aac2a08b155dfd8b3069388d0
Hannah10262016 <hyalavarthi@icloud.com>
2026-05-03 20:12:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function aboutme (){
+    window.open("index.html","_self");   
+} 
+
+function myart (){
+    window.open("art.html","_self");
+}
+
+function myprojects (){
+    window.open("projects.html","_self");
+        
+}
